bac6341cfedea719257429fa580736e778ef3c35
Felix Mehrhoff <felix.mehrhoff@eonerc.rwth-aachen.de>
2022-05-12 17:12:30 +0200

    added loop

--- reversibleBerechnung.py
+++ reversibleBerechnung.py
@@ -1,38 +1,50 @@
 
 #benötigte Variablen
-Q_ganzesHaus = 2.557 #kW
+Q_ganzesHaus = [2.557, 2.884] #kW
 T_vorlauf = 70  #°C
 T_ruecklauf = 36.5 #°C
 cp = 4.18 #kJ/(kg*K)
-Tm_12 = 10 #°C, vielleicht die Außentemperatur, Verdampfung nur im Nassdampf angenommen
+Tm_12 = 10 #°C, vielleicht die Außentemperatur, Verdampfung nur im Nassdampfgebiet angenommen
 s4 = 1.384 #kJ/kg*K
 s3 = 1.7716 #kJ/kg*K
 strompreis=0.4 #€/kWh
 betriebstage=340
+zaehler = 1
 
-#Energiebilanz um Heizung;
-m_heizkreislauf = Q_ganzesHaus/(cp*(T_vorlauf-T_ruecklauf)) # ist ideale Flüssigkeit i.O.?
-
-
-#kleines q berechnen
-q_waermebedarf = Q_ganzesHaus/m_heizkreislauf
+for i in Q_ganzesHaus:
 
 
-#Entropiebilanz um Wärmeübertrager, isentrop
-Tm_34 = -q_waermebedarf/(s4-s3)
+    #Energiebilanz um Heizung;
+    m_heizkreislauf = i/(cp*(T_vorlauf-T_ruecklauf)) # ist ideale Flüssigkeit i.O.?
 
-#COP berechnen und ausgeben
-COP = 1/(1-(Tm_12+273.15)/(Tm_34))
-print("Der COP der Wärmepumpe beträgt: " + str(COP) + ".")
 
-#Wärmepumpenleistung berechnen und ausgeben
-P_el = Q_ganzesHaus/COP
-print("Die Wärmepumpenleistung beträgt: " + str(P_el) + " kW.")
+    #kleines q berechnen
+    q_waermebedarf = i/m_heizkreislauf
 
-#Stromkosten berechnen und ausgeben
-kosten= P_el*strompreis*24*betriebstage
-if betriebstage<356:
-    print("Die jährlichen Stromkosten entsprechen bei " + str(betriebstage) + " Betriebstagen: " + str(kosten) + "€.")
-else:
-    print("Die jährlichen Stromkosten entsprechen bei Dauerbetrieb " + str(round(kosten,2)) + "€.")
+
+    #Entropiebilanz um Wärmeübertrager, isentrop
+    Tm_34 = -q_waermebedarf/(s4-s3)
+
+    #Tag angeben:
+
+    print("Für Tag " + str(zaehler) + " gilt: ")
+    # Zähler hochzählen
+    zaehler = zaehler+1
+
+    #COP berechnen und ausgeben
+    COP = 1/(1 - (Tm_12+273.15) / Tm_34)
+    print("Der COP der Wärmepumpe beträgt: " + str(COP) + ".")
+
+    #Wärmepumpenleistung berechnen und ausgeben
+    P_el = i/COP
+    print("Die Wärmepumpenleistung beträgt: " + str(P_el) + " kW.")
+
+
+
+    #Stromkosten berechnen und ausgeben
+    kosten= round(P_el*strompreis*24*betriebstage,2)
+    if betriebstage < 356:
+        print("Die jährlichen Stromkosten entsprechen bei " + str(betriebstage) + " gleichen Betriebstagen: " + str(kosten) + "€.")
+    else:
+        print("Die jährlichen Stromkosten entsprechen bei Dauerbetrieb mit 365 gleichen Betriebstagen" + str(kosten) + "€.")
 
