6d9d52757fb85c34b4973f4f476e415b5222ed65
Felix Mehrhoff <felix.mehrhoff@eonerc.rwth-aachen.de>
2022-05-16 14:37:19 +0200

    improved output and added Excel-file

--- reversibleBerechnung.py
+++ reversibleBerechnung.py
@@ -1,21 +1,26 @@
 
+import xlsxwriter
+
+
 #benötigte Variablen
 Q_ganzesHaus = [2.557, 2.884] #kW
 T_vorlauf = 70  #°C
 T_ruecklauf = 36.5 #°C
-cp = 4.18 #kJ/(kg*K)
+cp_wasser = 4.18 #kJ/(kg*K)
 Tm_12 = 10 #°C, vielleicht die Außentemperatur, Verdampfung nur im Nassdampfgebiet angenommen
 s4 = 1.384 #kJ/kg*K
 s3 = 1.7716 #kJ/kg*K
 strompreis=0.4 #€/kWh
 betriebstage=340
 zaehler = 1
+ergebnis = []
+
 
 for i in Q_ganzesHaus:
 
 
     #Energiebilanz um Heizung;
-    m_heizkreislauf = i/(cp*(T_vorlauf-T_ruecklauf)) # ist ideale Flüssigkeit i.O.?
+    m_heizkreislauf = i/(cp_wasser*(T_vorlauf-T_ruecklauf)) # ist ideale Flüssigkeit i.O.?
 
 
     #kleines q berechnen
@@ -23,13 +28,12 @@ for i in Q_ganzesHaus:
 
 
     #Entropiebilanz um Wärmeübertrager, isentrop
-    Tm_34 = -q_waermebedarf/(s4-s3)
+    Tm_34 = q_waermebedarf/(s3-s4)
 
     #Tag angeben:
 
     print("Für Tag " + str(zaehler) + " gilt: ")
-    # Zähler hochzählen
-    zaehler = zaehler+1
+
 
     #COP berechnen und ausgeben
     COP = 1/(1 - (Tm_12+273.15) / Tm_34)
@@ -42,9 +46,36 @@ for i in Q_ganzesHaus:
 
 
     #Stromkosten berechnen und ausgeben
-    kosten= round(P_el*strompreis*24*betriebstage,2)
-    if betriebstage < 356:
-        print("Die jährlichen Stromkosten entsprechen bei " + str(betriebstage) + " gleichen Betriebstagen: " + str(kosten) + "€.")
-    else:
-        print("Die jährlichen Stromkosten entsprechen bei Dauerbetrieb mit 365 gleichen Betriebstagen" + str(kosten) + "€.")
+    kosten = P_el*strompreis
+    print("Die Kosten pro Stunde betragen: " + str(round(kosten, 2)) + " €/kWh")
+
+    list =[zaehler, round(COP, 2), round(P_el, 2), round(kosten, 2)]
+    ergebnis.append(list)
+
+
+    # Zähler hochzählen
+    zaehler = zaehler + 1
+
+print(ergebnis)
+
+#Excel-Datei inizieren
+workbook = xlsxwriter.Workbook('Auswertung.xlsx')
+worksheet = workbook.add_worksheet()
+worksheet.write('A1', 'Tag')
+worksheet.write('B1', 'COP')
+worksheet.write('C1', 'elektrische Leistung [kW]')
+worksheet.write('D1', 'Preis [€/kWh]')
+
+row=1
+col=0
+
+
+for day, cop, power, price in ergebnis:
+    worksheet.write(row, col, day)
+    worksheet.write(row, col+1, cop)
+    worksheet.write(row, col+2, power)
+    worksheet.write(row, col+3, price)
+    row += 1
+
+workbook.close()
 
