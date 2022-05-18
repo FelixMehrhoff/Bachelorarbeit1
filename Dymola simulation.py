0fddbaa07083e11232a3f7de7eb4869f7eccc539
Felix Mehrhoff <felix.mehrhoff@eonerc.rwth-aachen.de>
2022-05-18 11:58:35 +0200

    added comments at DymolaSimulation and created TimeSeriesData

--- Dymola simulation.py
+++ Dymola simulation.py
@@ -1,36 +1,39 @@
+
+# import all relevant packages
 import os.path
 from dymola.dymola_interface import DymolaInterface
 
+# start Dymola
 dymola = DymolaInterface()
 
+# determine paths of the relevant packages
 dir_aixlib = "D:/hkr-fme/Projects/AixLib/AixLib"
-
 dir_model = "C:/Users/hkr-fme/TEASEROutput/BA"
-
 dir_weaData = "C:/Users/hkr-fme/TEASEROutput/BA"
-
 dir_result = 'D:/hkr-fme/Projects/Bachelorarbeit1/Results'
 
+# open models
 dymola.openModel(path=os.path.join(dir_aixlib, 'package.mo'))
 dymola.openModel(path=os.path.join(dir_model, 'package.mo'))
 dymola.openModel(path=os.path.join(dir_weaData, 'DEU_BW_Mannheim_107290_TRY2010_12_Jahr_BBSR'))
 
+# translate model, which should be simulated
 dymola.translateModel('BA.InstitutVersuchshalle.InstitutVersuchshalle')
-overview = []
-for i in range(0, 2):
-    output = dymola.simulateExtendedModel(
+
+# simulate model
+output = dymola.simulateExtendedModel(
         problem='BA.InstitutVersuchshalle.InstitutVersuchshalle',
-        startTime=86400*i,
-        stopTime=86400*(i+1),
+        startTime=0,
+        stopTime=3.1536e+07,
         outputInterval=3600,
         method="Dassl",
         tolerance=0.0001,
         resultFile=os.path.join(dir_result, 'results'),
-        finalNames=['multizone.TRad[1]', 'multizone.TRad[2]'],
-    )
-    overview.append(output[1])
+        finalNames=['multizone.TRad[1]'],
+        )
 
+# close Dymola
 dymola.close()
-print(overview)
-print(output)
 
+# if output[0] == True, the simulation was successful
+print(output)
